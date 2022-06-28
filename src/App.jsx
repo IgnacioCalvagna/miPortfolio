6abdc6f444457f5141d76413d22ad4fbb5a2dc9d
IgnacioCalvagna <ignaciocalvagna@gmail.com>
2022-06-27 21:50:54 -0300

    vuelta a cero y deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
 import "./assets/css/App.css";
-import AboutMe from "./components/AboutMe";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Proyects from "./components/Proyects";
-import ContactMe from "./components/ContactMe";
+// import AboutMe from "./components/AboutMe";
+// import Header from "./components/Header";
+// import Navbar from "./components/Navbar";
+// import Proyects from "./components/Proyects";
+// import ContactMe from "./components/ContactMe";
 // import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <div className="App">
-        <Header />
-        <div id="aboutMe">
-          <AboutMe />
-        </div>
-        <div id="proyects">
-          <Proyects />
-        </div>
-        <div id="contactMe">
-          <ContactMe />
-        </div>
-        {/* <Footer /> */}
-      </div>
+     <h1>Mi portfolio </h1>
     </>
   );
 }
