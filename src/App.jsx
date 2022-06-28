286d415c16c9fd081e00e346de513551cf41fdaf
IgnacioCalvagna <ignaciocalvagna@gmail.com>
2022-06-27 22:03:05 -0300

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./assets/css/App.css";
 // import AboutMe from "./components/AboutMe";
 // import Header from "./components/Header";
-// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 // import Proyects from "./components/Proyects";
 // import ContactMe from "./components/ContactMe";
 // import Footer from "./components/Footer";
@@ -9,6 +9,7 @@ import "./assets/css/App.css";
 function App() {
   return (
     <>
+    <Navbar/>
      <h1>Mi portfolio </h1>
     </>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+const navbar = () => {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Ignacio Calvagna</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#link">About me</Nav.Link>
+              <Nav.Link href="#link">Proyects</Nav.Link>
+              <Nav.Link href="#link">Contact me</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default navbar;
