aa593058247e2e21209a7e260064729c26e44243
IgnacioCalvagna <ignaciocalvagna@gmail.com>
2022-06-30 11:38:31 -0300

    agregando nuevo proyecto e-commerce

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -3,19 +3,21 @@ import fakeData from "../fakeData.json";
 import sativaCo from "../assets/img/proyectos/sativaCo.png";
 import imasApp from "../assets/img/proyectos/ImasApp.jpg";
 import satindica from "../assets/img/proyectos/satindica.jpg";
+import gif from "../assets/img/proyectos/gift-experience.jpg";
 import "../assets/css/proyects.css";
 const Proyects = () => {
 
-  const imgProyects = [sativaCo,imasApp,satindica]
+  const imgProyects = [sativaCo,imasApp,satindica,gif]
   const misProyectos = fakeData.proyectos.map((p,i) => {
     return (
       <div className="item-producto "key={i}>
         <div key={p.id} className="desc-producto">
           <h2>{p.titulo}</h2>
-          <p>{p.description}</p>
-        </div>
-        <div className="img-producto">
           <img src={imgProyects[i]} width="100%" alt="" />
+        
+        </div>
+        <div className=" img-producto">
+          <p>{p.description}</p>
         </div>
       </div>
     );
