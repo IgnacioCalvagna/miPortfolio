c669cc636a99332c0fdb7387d6b7fd1adfe6e4d7
Ignacio Calvagna <ignaciocalvagna@gmail.com>
2022-09-01 11:56:23 -0300

    finish

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,22 +7,40 @@ import ig from "../assets/img/ig.png";
 
 const Footer = () => {
   return (
-    <>
+    <section className="footerTot">
       <div className="download">
-        <button className=" btn btn-dark">
+        <button className=" btn btn-dark b">
           <a href={cv} download="Cv-Ignacio-Calvagna.pdf">
             Descargar CV
           </a>
         </button>
       </div>
 
-      <div className="toti">
+      <section className="infoFooter">
+        <div className="donde">
+          <p>
+            {" "}
+            Buenos Aires <br />
+            Argentina
+          </p>
+        </div>
+
         <div className="redes" style={{ float: "left" }}>
           <p>Podes encontrarme en :</p>
-          <img className="logo" src={github} width="3%" alt="GitHub" />
-          <img className="logo" src={lin} width="3%" alt="Linkedin" />
-          <img className="logo" src={ig} width="2.5%" alt="Instagram" />
+
+          <a href="https://github.com/IgnacioCalvagna/">
+            <img className="logo" src={github} width="8%" alt="GitHub" />
+          </a>
+          <a href="https://www.linkedin.com/in/ignacio-calvagna/">
+            <img className="logo" src={lin} width="6.8%" alt="Linkedin" />
+          </a>
+          <a href="https://www.instagram.com/nachitocal/">
+          <img className="logo" src={ig} width="5.5%" alt="Instagram" />
+          </a>
         </div>
+      </section>
+
+      <div className="toti">
         <div className="pDelFooter">
           <p>
             Portfolio diseñado y desarrollado por Ignacio Calvagna <br />
@@ -31,7 +49,7 @@ const Footer = () => {
         </div>
       </div>
       <br />
-    </>
+    </section>
   );
 };
 
